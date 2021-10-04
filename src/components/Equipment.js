d840a974f46426b01f4058b02ebfa933cdf7d03f
Abby Anderson <abby.e.anderson@gmail.com>
2021-10-04 11:14:23 -0600

    created components and tied them together with switch

---
+++ src/components/Equipment.js
@@ -0,0 +1,9 @@
+//here's where we track all the stuff your character is holding, including weapons, money, magic stuff, etc
+
+import React from "react";
+
+function Equipment () {
+    return <div>equipment</div>
+}
+
+export default Equipment;
---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+    <div>
+      <NavLink
+      to=""
+      >
+        Home 
+      </NavLink>
+  
+  
+      <NavLink
+      to="/characterBio"
+      >
+        CharacterBio 
+      </NavLink>
+  
+  
+      <NavLink
+      to="/abilityScores"
+      >
+        AbilityScores
+      </NavLink>
+  
+  
+      <NavLink
+      to="/equipment"
+      >
+        Equipment
+      </NavLink>
+  
+      <NavLink
+      to="/spells"
+      >
+        Spells
+      </NavLink>
+  
+    </div>
+    )
+  }
+  
+  export default NavBar;
