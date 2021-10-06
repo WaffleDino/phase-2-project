3b77061737078cecf3d5dc740d50b1e345d8438a
Abby Anderson <abby.e.anderson@gmail.com>
2021-10-06 08:29:37 -0600

    a little more work on equipment component

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -6,6 +6,12 @@ import React, {useEffect, useState} from "react";
 
 function Equipment () {
     const [equipment, setEquipment] = useState ([])
+    const [chosenEquipment, setChosenEquipment] = useState([])
+    const [currency, setCurrency] =useState({
+        gold: 0,
+        silver: 0,
+        copper: 0,
+    })
 
     //fetch to get list of equipment from api
     useEffect (() => {
@@ -18,10 +24,15 @@ function Equipment () {
     }, [])
 
     const handleChange = (e) => {
-        console.log(e.target.name)
-        console.log(e.target.value)
-        //going to put these inside of an array and save the array with the key of equipment
+        console.log('target name', e.target.name)
+        console.log('target value', e.target.value)
+    }
 
+    const handleCurrencyChange = (e) => {
+        setCurrency({
+            ...currency,
+            [e.target.name]: e.target.value
+        })
     }
     
     const renderEquipment = () => {
@@ -46,14 +57,78 @@ function Equipment () {
         }
     }
 
-//then also going to add a form where user can type in their gold, silver, and other money
+    const renderCurrencyForm = () => {
+        return (
+        <form onSubmit={submitCurrency}>
+            <label> Gold:
+                <input type="text" name="gold" onChange={handleCurrencyChange} value={currency.gold}/>
+            </label>
 
-//then also going to add another form where user can type in their other items! 
+                
+            <label> Silver:
+                <input type="text" name="silver" onChange={handleCurrencyChange} value={currency.silver}/>
+            </label>
+
+                
+            <label> Copper:
+                <input type="text" name="copper" onChange={handleCurrencyChange} value={currency.copper}/>
+            </label>
+            
+            <input type="submit" />
+        </form>
+    )}
+
+    const submitCurrency = (e) => {
+        e.preventDefault();
+        console.log(currency)
+    }
+    
+    const submitOtherItems = (e) => {
+        e.preventDefault();
+        console.log(e.target.value)
+    }
+
+    const renderOtherForm = () => {
+        return (
+            <form onSubmit={submitOtherItems}>
+                <label>Other items:</label>
+                <textarea>
+                    
+                </textarea>
+
+
+
+            </form>
+        )
+    }
+
 
     return (
         <>
-            <div>equipment</div>
+            <p>equipment</p>
+
             {renderEquipment()}
+            <br />
+            {renderCurrencyForm()}
+            <br />
+            {/* {renderOtherForm()} */}
+            <br />
+
+            <p>You've chosen to carry:</p>
+            <ul>
+                Equipment:
+                {/* list items for chosen equipment
+                prob just make this into a funciton and call it here */}
+            </ul>
+            <ul>
+                Currency:
+
+            </ul>
+            <ul>
+                Other:
+
+            </ul>
+
         </>
     ) 
 }
