443a3bb344d4c1d319445c5a52bac045838809b6
Abby Anderson <abby.e.anderson@gmail.com>
2021-10-07 13:03:36 -0600

    updated completed sheet functionality

--- src/components/CompletedSheet.js
+++ src/components/CompletedSheet.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 
-function CompletedSheet ({characterInfo, finishedEquipment}) {
+function CompletedSheet ({characterInfo, finishedEquipment, selectedSpells}) {
     //will have all the completed info from previous components passed through to here as props
 
     //will display all those props on the page here
@@ -16,6 +16,13 @@ return (
     <div>Completed Sheet </div>
     {console.log('inside completed sheet', characterInfo)}
     {console.log('inside completed sheet', finishedEquipment)}
+    {console.log('inside completed sheet', selectedSpells)}
+    
+    <div>
+        <h3>selected spells</h3>
+    {selectedSpells.map(each => <p>{(each.value)}</p>)}
+    </div>
+
 
     <p>Name: {characterInfo.name}</p>
     <p>Class: {characterInfo.class}</p>
