313bf1369ad83da818bf45626c7b2a3ad8e2838b
Abby Anderson <abby.e.anderson@gmail.com>
2021-10-07 11:55:56 -0600

    noodling around in home component, added bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,49 @@
 //first page with a landing image, new character button
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { NavLink } from "react-router-dom";
+
 
 
 function Home () {
 
-    const fetchSavedSheets = () => {
+    const [savedSheets, setSavedSheets] = useState([]);
+
+    useEffect (() => {
         fetch('http://localhost:4000/characterbio')
         .then(response => response.json())
         .then(data => {
-            return (
-            data.map(each => {
-                return (
-                    <div>
-                    {console.log(each)}
-                    <p>{each.name}</p>
-                    {/* probably will need to make this a separate render function sadly */}
-                    </div>
-                )
-            })
-        )}
-        )
+            console.log('inside home', data)
+            setSavedSheets(data)
+        }) 
+    }, []) //should make this go every time there's a toggle state change
+    
+    const renderSavedSheets = () => {
+        //triggered on button click, should take the savedSheets state and render maybe the character name
+        console.log(savedSheets)
     }
-                
-  
+
 
     return (
     <>
     <div>home</div>
 
+
+
+    <div className="row">
+
+    <div className="col-xs-12">
+        <img className="img-fluid"src="https://via.placeholder.com/1000"/>
+    </div>
+
+    <div className="col-sm-12 col-md-6">
+        <p>Are you looking for a lightweight, minimalist character sheet? Welcome to your new favorite app. Just fill in your character's info in the following pages and enjoy the result. </p>
+    </div>
+    </div>
         <NavLink to="characterBio">
             <button>Create new character sheet</button>
         </NavLink>
-    <button onClick={fetchSavedSheets}>Review saved character sheets</button>
+    <button onClick={renderSavedSheets}>Review saved character sheets</button>
 
     </>
     )
